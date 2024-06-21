b085be9bc344be5dcc11a0cc6df71e88a231bcd1
anjakely18 <anjaniaina03@gmail.com>
2024-06-21 18:43:48 +0200

    animation et son sequences

--- game.js
+++ game.js
@@ -1,18 +1,24 @@
 // Tableau contenant les couleurs du jeu.
-const buttonColours = ["red", "blue", "green", "yellow"];
+var buttonColours = ["red", "blue", "green", "yellow"];
+
 // Tableau pour stocker la séquence de couleurs dans le jeu
-let gamePattern = [];
+var gamePattern = [];
 
 /**
  * Fonction pour générer la prochaine couleur dans la séquence
  */
 const nextSequence = () => {
-  // Génère un nombre aléatoire entre 0 et 3 (inclus)
-  let randomNumber = Math.floor(Math.random() * 3);
+  var randomNumber = Math.floor(Math.random() * 4);
 
-  // Sélectionne une couleur aléatoire du tableau buttonColours en utilisant le nombre aléatoire
-  const randomChosenColour = buttonColours[randomNumber];
-
-  // Ajoute la couleur aléatoire sélectionnée à la séquence de jeu
+  // Sélectionne une couleur aléatoire du tableau buttonColours en utilisant le nombre aléatoire et Ajoute la couleur aléatoire sélectionnée à la séquence de jeu
+  var randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  // Applique l'animation de clignotement (flash)
+  var $selectedButton = $("#" + randomChosenColour);
+  $selectedButton.fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
+
+  //Mettre le son
+  var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+  audio.play();
 };
