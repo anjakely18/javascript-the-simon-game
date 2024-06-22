a4390c369e0d4b3e36c8fcc18df94969ba797e24
anjakely18 <anjaniaina03@gmail.com>
2024-06-22 22:39:50 +0200

    ajout animation joueur

--- game.js
+++ game.js
@@ -4,10 +4,31 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 // Tableau pour stocker la séquence de couleurs dans le jeu
 var gamePattern = [];
 
+//Tableau pour stocker les boutons cliqués par le joueur
+var userClickedPattern = [];
+
+/**
+ * fonction pour mettre le son
+ * @param {} name
+ */
+function playSound(name) {
+  var audio = new Audio(`sounds/${name}.mp3`);
+  audio.play();
+}
+
+/**fonction pour animer la couleur choisie */
+function animatePress(currentColor) {
+  $(`#${currentColor}`).addClass("pressed");
+
+  setTimeout(function () {
+    $(`#${currentColor}`).removeClass("pressed");
+  }, 100);
+}
+
 /**
  * Fonction pour générer la prochaine couleur dans la séquence
  */
-const nextSequence = () => {
+function nextSequence() {
   var randomNumber = Math.floor(Math.random() * 4);
 
   // Sélectionne une couleur aléatoire du tableau buttonColours en utilisant le nombre aléatoire et Ajoute la couleur aléatoire sélectionnée à la séquence de jeu
@@ -19,6 +40,15 @@ const nextSequence = () => {
   $selectedButton.fadeOut(50).fadeIn(50).fadeOut(50).fadeIn(50);
 
   //Mettre le son
-  var audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-  audio.play();
-};
+  playSound(randomChosenColour);
+}
+
+// Vérifie quel bouton a été appuyé: userChosenColour contiendra l'id (le nom de la couleur)
+$(".btn").on("click", function () {
+  var userChosenColour = $(this).attr("id"); // Récupère l'ID du bouton cliqué
+
+  userClickedPattern.push(userChosenColour); // Ajoute userChosenColour à userClickedPattern
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+});
